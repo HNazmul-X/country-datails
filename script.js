04b6abafc5deb15cde1c08f59b5c0ed5236c8bf4
H Nazmul Hassan <71649134+HNazmul@users.noreply.github.com>
2021-02-06 20:41:11 +0600

    project added with out jonkar mahbub

---
+++ script.js
@@ -0,0 +1,91 @@
+const countriesBoxArea = document.getElementById('all-countries-area');
+
+let code = ""
+var idName = '';
+fetch("https://restcountries.eu/rest/v2/all")
+.then(res => res.json())
+.then(data => {
+  console.log(data)
+    for(let i =0; i < data.length; i ++){
+        const countriesbox = document.createElement("div");
+        countriesbox.className = "countries-box";
+        const countryName = document.createElement("h3");
+        const countrycapital = document.createElement("h5");
+        const viewDetailsBtn = document.createElement("button")
+        viewDetailsBtn.innerText = "view Datails"
+        viewDetailsBtn.id = data[i].alpha2Code;
+
+        countryName.innerText =data[i].name;
+        countrycapital.innerText = data[i].name
+
+        countriesBoxArea.appendChild(countriesbox);
+        countriesbox.appendChild(countryName);
+        countriesbox.appendChild(countrycapital)
+        countriesbox.appendChild(viewDetailsBtn)
+
+       
+
+    }
+
+    const datailsBtn  = document.querySelectorAll(".countries-box button");
+    for (let i = 0; i < datailsBtn.length; i++) {
+      datailsBtn[i].addEventListener("click", (e) => {
+         code = e.target.id
+          console.log(code);
+          const datailsUrl = `https://restcountries.eu/rest/v2/alpha/${code}`;
+
+          fetch(datailsUrl)
+          .then(res => res.json())
+          .then(data => {
+            console.log(data.name)
+          })
+
+      });
+    }
+    
+     
+})
+
+
+
+
+
+
+
+
+
+
+
+
+// /* create h1 item funtin */
+// const creatH1 = (text) =>{
+//     const h1 = document.createElement('h1')
+//     h1.textContent = text;
+//     return h1;
+// }
+// /* create h2 item funtin */
+// const createH2 = (text) =>{
+//     const h2 = document.createElement('h1')
+//     h2.textContent = text;
+//     return h2;
+// }
+
+// const appendChildren = (parents,children) =>{
+//     for(let i = 0; i < children.length; i ++){
+//         parents.appendChild(children[i]);
+//         children[i].id = idName
+//     }
+// }
+
+
+// const items = [
+//     creatH1('Dom'),
+//     creatH1("nazmul"),
+//     creatH1("sabina"),
+// ]
+
+// appendChildren(countriesBoxArea,items);
+
+
+
+
